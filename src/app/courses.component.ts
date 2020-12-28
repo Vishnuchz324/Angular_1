5f3744cd2ad9af49dc789dd74eb4a55a0f52b960
Vishnuchz324 <vishnucheruvakad@gmail.com>
2020-12-28 21:07:31 +0530

    till1:52

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 @Component({
   selector:"courses",
-  template:"<h2>{{getTitle()}}</h2>"
+  template:`
+  <h2>{{title}}</h2>
+  <ul>
+    <li *ngFor="let course of courses">
+    {{course}}
+    </li>
+  </ul>
+  `
 })
 export class CoursesComponent{
-title="Again my project";
-getTitle(){
-  return this.title;
-}
+title="Course List";
+courses=["Course1","Course2","Course3","Course4"];
 }
